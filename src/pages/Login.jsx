4af830f52bc4d3f8bf6389b3ad9f52d67b4d8aed
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-09 10:23:19 +0100

    update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,34 +1,60 @@
 import { CiMail } from "react-icons/ci";
-import Logo from "../assets/Vidocean2.png";
 import { MdOutlinePassword } from "react-icons/md";
+import Logo from "../assets/Vidocean2.png";
+import { FcGoogle } from "react-icons/fc";
+import { BsApple } from "react-icons/bs";
+import { FaXTwitter } from "react-icons/fa6";
+
 const Login = () => {
   return (
-    <div className=" bg-[url('./assets/bg.svg')] w-full items-center justify-center mx-auto flex h-screen">
-      <div className=" bg-blue-500 rounded-lg p-2 flex flex-col text-center items-center md:w-1/2 w-full gap-3">
-        <div className="">
-          <img src={Logo} alt="" className=" h-10 w-20" />
-          <h2 className=" text-xl font-ubuntu font-medium">Welcome Back</h2>
+    <div className="bg-[url('./assets/bg.svg')] w-full flex items-center justify-center mx-auto h-screen">
+      <div className="bg-secondary bg-opacity-10   bg-clip-padding backdrop-filter backdrop-blur-sm rounded-lg border border-secondary p-6 flex flex-col text-center items-center md:w-1/3 w-full gap-6">
+        <div className="flex flex-col items-center gap-2">
+          <img src={Logo} alt="Vidocean Logo" className="h-12 w-28" />
+          <h2 className="text-2xl font-ubuntu font-medium">Welcome Back</h2>
           <p className="text-sm font-pop">
-            Dont have an account? <span> Sign Up</span>
+            Don&apos;t have an account?{" "}
+            <span className="text-secondary">Sign Up</span>
           </p>
         </div>
-        <div className=" flex gap-4 flex-col">
-          <div className=" flex p-2 border border-text gap-2 rounded-md w-full">
-            <CiMail className=" w-6 h-6 text-text " />
+
+        <div className="flex flex-col w-full gap-4">
+          <div className="flex p-2 border border-text gap-2 rounded-md w-full items-center">
+            <CiMail className="w-6 h-6 text-gray-500" />
             <input
-              type="text"
-              placeholder="Email Adress"
-              className=" bg-transparent placeholder:text-text placeholder:font-pop font-pop text-sm"
+              type="email"
+              placeholder="Email Address"
+              className="bg-transparent placeholder:text-gray-500 placeholder:font-pop font-pop text-sm w-full outline-none"
             />
           </div>
 
-          <div className=" flex p-2 border border-text gap-2 rounded-md w-full">
-            <MdOutlinePassword className=" w-6 h-6 text-text " />
+          <div className="flex p-2 border border-text gap-2 rounded-md w-full items-center">
+            <MdOutlinePassword className="w-6 h-6 text-gray-500" />
             <input
-              type="text"
+              type="password"
               placeholder="Password"
-              className=" bg-transparent placeholder:text-text placeholder:font-pop font-pop text-sm"
+              className="bg-transparent placeholder:text-gray-500 placeholder:font-pop font-pop text-sm w-full outline-none"
             />
+          </div>
+          <div className="bg-secondary p-2 text-primary font-pop rounded-md cursor-pointer hover:bg-secondary-dark hover:text-white active:scale-95 transition-all duration-300 ease-in-out">
+            LogIn
+          </div>
+        </div>
+        <div className="w-full flex items-center gap-4">
+          <hr className="flex-grow border-t border-text" />
+          <span className="text-text font-pop text-sm">or</span>
+          <hr className="flex-grow border-t border-text" />
+        </div>
+
+        <div className=" flex gap-6">
+          <div className=" bg-transparent  border-2 border-secondary p-2 rounded-md">
+            <BsApple className="h-6 w-6" />
+          </div>
+          <div className="bg-transparent  border-2 border-secondary p-2 rounded-md">
+            <FcGoogle className="h-6 w-6" />
+          </div>
+          <div className="bg-transparent  border-2 border-secondary p-2 rounded-md">
+            <FaXTwitter className="h-6 w-6" />
           </div>
         </div>
       </div>
