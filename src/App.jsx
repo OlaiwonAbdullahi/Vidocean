f6c2cf82af919975ebdc4845f047033b649bfcf5
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-09 08:59:12 +0100

    update App.jsx, Login.jsx and SignUp.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Login from "./pages/Login";
+
 const App = () => {
-  return <div className="font-ubuntu">App</div>;
+  return (
+    <div className="font-ubuntu">
+      <Login />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Login;
