aba75a8c5aa28f3fb19e2006c59d1be0a8310b26
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-09 13:21:29 +0100

    update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,19 +6,52 @@ import { BsApple } from "react-icons/bs";
 import { FaXTwitter } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
-const SignUp = () => {
+const Signup = () => {
   return (
     <div className="bg-[url('./assets/bg.svg')] bg-cover bg-center w-full flex items-center justify-center mx-auto h-screen">
       <div className="bg-secondary bg-opacity-10   bg-clip-padding backdrop-filter backdrop-blur-sm rounded-lg border border-secondary p-6 flex flex-col text-center items-center md:w-1/3 w-full gap-6">
         <div className="flex flex-col items-center gap-2">
           <img src={Logo} alt="Vidocean Logo" className="h-12 w-28" />
-          <h2 className="text-2xl font-ubuntu font-medium">Welcome Back :) </h2>
+          <h2 className="text-2xl font-ubuntu font-medium">
+            {" "}
+            Create an Account for Free (**){" "}
+          </h2>
           <p className="text-sm font-pop">
-            Don&apos;t have an account?{" "}
-            <Link to="/signin">
-              <span className="text-secondary cursor-pointer">Sign Up</span>
+            Already have an account?{" "}
+            <Link to="/">
+              <span className="text-secondary cursor-pointer">Login</span>
             </Link>
           </p>
+        </div>
+
+        <div className=" flex flex-col gap-3">
+          <div className="bg-transparent  border-2 border-secondary p-2 rounded-md cursor-pointer hover:bg-primary flex gap-2">
+            <FcGoogle className="h-4 w-4 mt-1" />
+            <span className=" whitespace-nowrap font-pop font-medium text-sm">
+              Sign up with Google
+            </span>
+          </div>
+          <div className=" bg-transparent  border-2 border-secondary p-2 rounded-md cursor-pointer hover:bg-primary flex gap-2">
+            <BsApple className="h-4 w-4 mt-1" />
+            <span className=" whitespace-nowrap font-pop font-medium text-sm">
+              Sign up with Apple
+            </span>
+          </div>
+
+          <div className="bg-transparent  border-2 border-secondary p-2 rounded-md cursor-pointer hover:bg-primary flex gap-2">
+            <FaXTwitter className="h-4 w-4 mt-1" />
+            <span className=" whitespace-nowrap font-pop font-medium text-sm">
+              Sign up with X
+            </span>
+          </div>
+        </div>
+
+        <div className="w-full flex items-center gap-4">
+          <hr className="flex-grow border-t border-text" />
+          <span className="text-text font-pop text-sm">
+            or Sign Up with Email
+          </span>
+          <hr className="flex-grow border-t border-text" />
         </div>
 
         <div className="flex flex-col w-full gap-4">
@@ -40,24 +73,7 @@ const SignUp = () => {
             />
           </div>
           <div className="bg-secondary p-2 text-primary font-pop rounded-md cursor-pointer hover:bg-secondary-dark hover:text-white active:scale-95 transition-all duration-300 ease-in-out">
-            LogIn
-          </div>
-        </div>
-        <div className="w-full flex items-center gap-4">
-          <hr className="flex-grow border-t border-text" />
-          <span className="text-text font-pop text-sm">or</span>
-          <hr className="flex-grow border-t border-text" />
-        </div>
-
-        <div className=" flex gap-6">
-          <div className=" bg-transparent  border-2 border-secondary p-2 rounded-md cursor-pointer hover:bg-primary">
-            <BsApple className="h-6 w-6" />
-          </div>
-          <div className="bg-transparent  border-2 border-secondary p-2 rounded-md cursor-pointer hover:bg-primary">
-            <FcGoogle className="h-6 w-6" />
-          </div>
-          <div className="bg-transparent  border-2 border-secondary p-2 rounded-md cursor-pointer hover:bg-primary">
-            <FaXTwitter className="h-6 w-6" />
+            Sign Up
           </div>
         </div>
       </div>
@@ -65,4 +81,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default Signup;
