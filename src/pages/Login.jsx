9ff35e052e19222d93830de72f36f3ae51b22967
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-09 09:18:57 +0100

    build(deps): update package-lock.json, package.json, Login.jsx and tailwind.config.js

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,23 @@
+import { CiMail } from "react-icons/ci";
+import Logo from "../assets/Vidocean1.png";
 const Login = () => {
   return (
-    <div>
-      <div className=""></div>
+    <div className=" bg-[url('./assets/bg.svg')] ">
+      <div className="">
+        <div className="">
+          <img src={Logo} alt="" />
+          <h2>Welcome Back</h2>
+          <p>
+            Dont have an account? <span> Sign Up</span>
+          </p>
+        </div>
+        <div className="">
+          <div className=" flex p-2 border border-text gap-2">
+            <CiMail />
+            <input type="text" placeholder="Email Adress" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
