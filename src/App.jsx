68e5f6773f7e0f1e3d4ac1d08e98f8e275ecf1cf
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-09 15:46:13 +0100

    update App.jsx and src/pages/Index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Login from "./pages/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
+import Index from "./pages/Index";
 const App = () => {
   return (
     <div className="">
@@ -8,7 +9,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signin" element={<SignUp />} />
-          <Route path="/signin" element={<SignUp />} />
+          <Route path="/index" element={<Index />} />
         </Routes>
       </Router>
     </div>
