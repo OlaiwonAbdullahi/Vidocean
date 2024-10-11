4240a91a84261c98b6cb423de1ae05c0d1d683fc
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-11 12:27:12 +0100

    update .env and fetchFormAPI.js

--- src/utils/fetchFormAPI.js
+++ src/utils/fetchFormAPI.js
@@ -1,20 +1,9 @@
-import axios from "axios";
-
-const BASE_URL = "https://youtube-v31.p.rapidapi.com/";
-
+const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
+const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
-  params: {
-    maxResults: "50",
-  },
+  method: "GET",
   headers: {
-    "x-rapidapi-key": "b8ef490beemshc78108332381208p1cc891jsn167f1fd3a95e", // This will fetch from .env
-    "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+    "x-rapidapi-key": API_KEY,
+    "x-rapidapi-host": "youtube138.p.rapidapi.com",
   },
-};
-
-export const fetchFromAPI = async (url) => {
-  // Check if the key is loading
-  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-
-  return data;
 };
