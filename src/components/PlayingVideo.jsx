7c82da0c8b5518208db68984dda5439fd448477b
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-12 10:04:28 +0100

    build(deps): update package-lock.json, package.json and PlayingVideo.jsx

--- src/components/PlayingVideo.jsx
+++ src/components/PlayingVideo.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchData } from "../utils/fetchFormAPI";
+import ReactPlayer from "react-player";
 
 const PlayingVideo = () => {
   const [video, setVideo] = useState();
@@ -16,7 +17,24 @@ const PlayingVideo = () => {
       console.log(res);
       setVideo(res);
     });
-  return <div>PlayingVideo</div>;
+  return (
+    <div className=" flex justify-center flex-row h-[calc(100%-56px)] mt-16">
+      <div className=" w-full max-w-[1500px] flex flex-col lg:flex-row">
+        <div className=" flex flex-col lg:w-[calc(100%-350px)] xl:w-[100%-400px] px-4 py-3 lg:py-6">
+          <div className=" h-[200px] md:h-[700px] ml-[-16px] mr-[-16px] lg:ml-0 lg:mr-0">
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              height="100%"
+              width="100%"
+              controls
+              style={{ backgroundColor: "#000000" }}
+              playing={true}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlayingVideo;
